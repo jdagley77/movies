2ac333b6bb06b81a1298efdcfdbb676e8e127ea3
Jack Dagley <jack.dagley77@gmail.com>
2017-02-19 12:29:05 -0800

    Create index for categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,7 +3,14 @@ class CategoriesController < ApplicationController
 	def index
 		@categories = Category.all
 	end
+	
+	def show
+		@category = Category.find(params[:id])
+		@movies = Movie.all
+	end
 
 	def new
 	end
+
+
 end
