a8327beade40ea33b7e98b21df59949aa70e5495
Jack Dagley <jack.dagley77@gmail.com>
2017-02-19 12:29:41 -0800

    Create index for movies, add links

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,3 +1,12 @@
 class MoviesController < ApplicationController
 	
+	def index 
+		@category = Category.find(params[:category_id])
+		@movies = @category.movies
+	end
+
+	def show
+		@movie = Movie.find(params[:id])
+		@category = Category.find(params[:category_id])
+	end
 end
